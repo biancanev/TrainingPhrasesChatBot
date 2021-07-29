28c03066fa0163f8ba381e368efc24d565231cf9
Biancano <bebostunner234@gmail.com>
2021-07-29 21:54:39 +0000

    added more information to the repository and fixed some minor bugs

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ This program generates tagged phrases for a network-technical chatbox. It can ge
 | 2 | Creates a phrase that gives all `attribute` an `inventory` had at `time` |
 | 3 | Creates a phrase that shows the `time` an `inventory` had `attribute` |
 | 4 | Creates a phrase that shows how many `inventory` had `attribute` at `time` |
-| 5 | Creates a phrase that shows the `time` an `inventory` had `attribute` |
+| 5 | Creates a phrase that shows how many `attribute` an `inventory` had at `time` |
 
 
 This can be achieved through the use of a single function
 ### Function and Parameters:
-This program uses the function `genQuestions(category1, category2, category3, category4, category5, objectParam, objectList, dateType)`. Below is a description of each parameter:
+This program uses the function `genQuestions(category1, category2, category3, category4, category5, objectParam, objectList, dateType)`. Currently, it is located on line 141. Below is a description of each parameter:
 
 | Parameter | Use |
 | --- | --- |
@@ -24,17 +24,34 @@ This program uses the function `genQuestions(category1, category2, category3, ca
 | `category5`(int) | Determines how many `Category 5` phrases the generator should generate. Default is `0`.|
 | `objectParam`(str) | Determines whether the program should include `objects` during generation. Acceptable parameters include: `"none"`, `""random"`, and `"given"`. `"none"` will not use objects when generating phrases. `"random"` will call the function `genObjects()` and generate a random string for an object 20% of the time. The rest will be a normal output with no object. `"given"` will iterate through the given list in `objectList` to create a unique phrase. Default is `"random"`|
 | `objectList`(tup) | If `objectParam` is `"given"`, the generator will iterate through the given list in category n given `(n, [])`. Default is `(0, [])`.|
-| `dateType`(int) | Sets the format of dates used in generating times. `1` is MM/DD/YYYY. `2` is DD/MM/YYYY. `3` is YYYY/MM/DD. `4` is MM DD, YYYY. Default is `4` |
+| `dateType`(int) | Sets the format of dates used in generating times. `1` is MM/DD/YYYY. `2` is DD/MM/YYYY. `3` is YYYY/MM/DD. `4` is MM DD, YYYY. `5` chooses a random selection of all the previous dateTypes. Default is `5`. |
 
 Therefore this code:
 ```python
-genQuestions(cat1=1, cat3=1, objectParam="given", objectList=(1,["VM1"]))
+>>>main.py
+
+genQuestions(category1=3, category3=1 ,objectParam="random", dateType=3)
 ```
 will output something like:
 ```
-Category 1 Phrases:
-What are the {VMs|class|primary} correlated to {VM1|object|secondary} in {deleted|modification|attribute} at {April 21, 2021|time}?
+Category 1 Questions:
+Show me the {faults|modification|attribute} of the {security groups|class|primary} pertaining to {THLJyapgnQ|object|secondary} on {2021/1/19}
+Find all {faults|modification|attribute} {security groups|class|primary} at 2021/1/19|sys_time}.
+Access all {faults|modification|attribute} {security groups|class|primary} at 2021/1/19|sys_time}.
+Category 3 Questions:
+At what time did {rules|class|primary} have {events|modification|attribute}
+```
 
-Category 3 Phrases:
-Were there any times when {security groups|class|primary} pertaining to {sec_group3} had {faults|modification|attribute}
+In addition to printing out the phrases, the generator also returns the phrases in a list. If you want to save the information to a text file, you may do as such:
+```python
+>>>main.py
+
+with open("file.txt","a") as f:
+  f.write(genQuestions(category1=3, category3=1 ,objectParam="random", dateType=3))
+```
+which will generate the file `file.txt` as such:
+```
+>>>file.txt
+
+['Show me the {faults|modification|attribute} of the {security groups|class|primary} pertaining to {THLJyapgnQ|object|secondary} on {2021/1/19}', 'Find all {faults|modification|attribute} {security groups|class|primary} at 2021/1/19|sys_time}.', 'Access all {faults|modification|attribute} {security groups|class|primary} at 2021/1/19|sys_time}.', 'At what time did {rules|class|primary} have {events|modification|attribute}']
 ```
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 inventory = ["VMs", "security groups", "NICs", "route tables", "rules", "networks", "CIDRs", "subnets", "subscriptions", "resource groups"]
 attribute = ["location", "status", "utilization", "events", "faults", "cost", "created", "deleted"]
 timeStamp = ["yesterday", "last week", "last month"]
-month = ["January", "February", "March"]
+month = ["January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
 def genDate(dateType):
   if dateType == 1:
     return (str(random.randint(1, 12)) + "/" + str(random.randint(1, 31)) + "/2021")
@@ -14,10 +14,19 @@ def genDate(dateType):
   elif dateType == 3:
     return ("2021/" + str(random.randint(1,12)) + "/" + str(random.randint(1,31)))
   elif dateType == 4:
-    return (str())
+    return (month[random.randint(0,23)] + str(random.randint(1,31) + ", 2021"))
+  elif dateType == 5:
+    rand = random.randint(1, 4)
+    if rand == 1:
+      return (str(random.randint(1, 12)) + "/" + str(random.randint(1, 31)) + "/2021")
+    elif rand == 2:
+      return (str(random.randint(1, 31)) + "/" + str(random.randint(1,12)) + "/2021")
+    elif rand == 3:
+      return ("2021/" + str(random.randint(1,12)) + "/" + str(random.randint(1,31)))
+    elif rand == 4:
+      return (month[random.randint(0,23)] + str(random.randint(1,31) + ", 2021"))
   else:
     print("dateType " + dateType + "is not valid")
-print(genDate(1))
 def genObject(length):
   name = ""
   lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -32,13 +41,14 @@ def genObject(length):
 def cat1(number, objectParam, dateType):
   data = []
   inventoryItem = inventory[random.randint(0, len(inventory)-1)]
-  attributeItem = attribute[random.randint(0, len(inventory)-1)]
+  attributeItem = attribute[random.randint(0, len(attribute)-1)]
   objectItem = genObject(random.randint(5, 12))
   timeItem = genDate(dateType)
   nonObjectQuestions = ["What are the {" + inventoryItem + "|class|primary} in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|time}?", "Can I see the {" + inventoryItem + "|class|primary} in {" + attributeItem + "|modification|attribute} at {" + timeItem + "?", "Show me all of {" + inventoryItem + "|class|primary} in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}.", "Give me information on {" + inventoryItem + "|class|primary} in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}.", "I need information on {" + inventoryItem + "|class|primary} in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}.", "Access all {" + inventoryItem + "|class|primary} in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}.", "Find all {" + inventoryItem + "|class|primary}  in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}." , "I want all {" + inventoryItem + "|class|primary} in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}.", "I want to see all {" + inventoryItem + "|class|primary} in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}.", "Can I access {" + inventoryItem + "|class|primary}  in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}?", "Open information on {" + inventoryItem + "|class|primary}  in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}.", "Send me information on {" + inventoryItem + "|class|primary} in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}.", "Report information on {" + inventoryItem + "|class|primary} in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}.", "Give me information on {" + inventoryItem + "|class|primary}  in {" + attributeItem + "|modification|attribute} at {" + timeItem + "|sys_time}.", "I want information on {" + inventoryItem + "|class|primary} in {" + attributeItem + "|modification|primary} at " + timeItem + "|sys_time}.", "Give me all {" + attributeItem + "|modification|attribute} {" + inventoryItem + "|class|primary} at " + timeItem + "|sys_time}.", "Access all {" + attributeItem + "|modification|attribute} {" + inventoryItem + "|class|primary} at " + timeItem + "|sys_time}.", "I want information on {" + attributeItem + "|modification|attribute} {" + inventoryItem + "|class|primary} at " + timeItem + ".", "Find all {" + attributeItem + "|modification|attribute} {" + inventoryItem + "|class|primary} at " + timeItem + "|sys_time}."]
-  objectQuestions = ["Give me the {" + attributeItem + "|modification|attribute} of the {" + inventoryItem + "|class|primary} that were associated to {" + objectItem + "|object|secondary} on {" + timeItem + "}"]
+  objectQuestions = ["Give me the {" + attributeItem + "|modification|attribute} of the {" + inventoryItem + "|class|primary} that were associated to {" + objectItem + "|object|secondary} on {" + timeItem + "}", "Show me the {" + attributeItem + "|modification|attribute} of the {" + inventoryItem + "|class|primary} that were associated to {" + objectItem + "|object|secondary} on {" + timeItem + "}", "I want access the {" + attributeItem + "|modification|attribute} to the {" + inventoryItem + "|class|primary} that were associated to {" + objectItem + "|object|secondary} on {" + timeItem + "}", "Show me the {" + attributeItem + "|modification|attribute} of the {" + inventoryItem + "|class|primary} pertaining to {" + objectItem + "|object|secondary} on {" + timeItem + "}", "Give me the {" + attributeItem + "|modification|attribute} of the {" + inventoryItem + "|class|primary} pertaining to {" + objectItem + "|object|secondary} on {" + timeItem + "}", "I want access to the {" + attributeItem + "|modification|attribute} of the {" + inventoryItem + "|class|primary} pertaining to {" + objectItem + "|object|secondary} on {" + timeItem + "}", "Open information on the {" + attributeItem + "|modification|attribute} of the {" + inventoryItem + "|class|primary} pertaining to {" + objectItem + "|object|secondary} on {" + timeItem + "}"]
   if objectParam == "none":
     for phrase in range(number):
+      objectItem = genObject(random.randint(5, 12))
       timeItem = genDate(dateType)
       data.append(nonObjectQuestions[random.randint(0, len(nonObjectQuestions)-1)])
     return data
@@ -57,7 +67,7 @@ def cat1(number, objectParam, dateType):
 def cat2(number):
   data = []
   inventoryItem = inventory[random.randint(0, len(inventory)-1)]
-  attributeItem = attribute[random.randint(0, len(inventory)-1)]
+  attributeItem = attribute[random.randint(0, len(attribute)-1)]
   timeItem = "Jan 1, 2021"
   questions = ["Show me the {" + attributeItem + "|modification|attribute} of {" + inventoryItem + "|class|primary} at {" + timeItem + "|sys_time}" + "?", "{" + attributeItem + "|modification|attribute} of {" + inventoryItem + "|class|primary} at {" + timeItem + "|sys_time}", "What is {" + attributeItem + "|modification|attribute} of {" + inventoryItem + "|class|primary} at {" + timeItem + "|sys_time}" + "?", "Show {" + attributeItem + "|modification|attribute} of {" + inventoryItem + "|class|primary} at {" + timeItem + "|sys_time}", "Which ones have {" + attributeItem + "|modification|attribute} of {" + inventoryItem + "|class|primary} at {" + timeItem + "|sys_time}", "Give me the {" + attributeItem + "|modification|attribute} of {" + inventoryItem + "|class|primary} at {" + timeItem + "|sys_time}", "Are there any {" + attributeItem + "|modification|attribute} of {" + inventoryItem + "|class|primary} at {" + timeItem + "|sys_time}", "Show the {"  + attributeItem + "|modification|attribute} of {" + inventoryItem + "|class|primary} at {" + timeItem + "|sys_time}"]
   for iteration in range(number):
@@ -67,7 +77,7 @@ def cat2(number):
 def cat3(number):
   data = []
   inventoryItem = inventory[random.randint(0, len(inventory)-1)]
-  attributeItem = attribute[random.randint(0, len(inventory)-1)]
+  attributeItem = attribute[random.randint(0, len(attribute)-1)]
   timeItem = "Jan 1, 2021"
   questions = ["When did {" + inventoryItem + "|class|primary} have {" + attributeItem + "|modification|attribute}", "What time did {" + inventoryItem + "|class|primary} have {" + attributeItem + "|modification|attribute}", "Give me a time when {" + inventoryItem + "|class|primary} had {" + attributeItem + "|modification|attribute}", "What times did {" + inventoryItem + "|class|primary} have {" + attributeItem + "|modification|attribute}", "At what time did {" + inventoryItem + "|class|primary} have {" + attributeItem + "|modification|attribute}", "Give me time when {" + inventoryItem + "|class|primary} had {" + attributeItem + "|modification|attribute}", "Were there any times when {" + inventoryItem + "|class|primary} had {" + attributeItem + "|modification|attribute}"]
   for iteration in range(number):
@@ -77,7 +87,7 @@ def cat3(number):
 def cat4(number):
   data = []
   inventoryItem = inventory[random.randint(0, len(inventory)-1)]
-  attributeItem = attribute[random.randint(0, len(inventory)-1)]
+  attributeItem = attribute[random.randint(0, len(attribute)-1)]
   timeItem = "Jan 1, 2021"
   questions = ["How many {" + inventoryItem + "|class|primary} were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "How much {" + inventoryItem + "|class|primary} were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "What number of {" + inventoryItem + "|class|primary} were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "How many {" + inventoryItem + "|class|primary} were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "Give me the number of {" + inventoryItem + "|class|primary} that were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "Give me the amount of {" + inventoryItem + "|class|primary} that were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "What amount of {" + inventoryItem + "|class|primary} were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "What is the size of {" + inventoryItem + "|class|primary} were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "What volume of {" + inventoryItem + "|class|primary} were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "What quantity of {" + inventoryItem + "|class|primary} were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "Give me the volume of {" + inventoryItem + "|class|primary} that were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "Give me the size of {" + inventoryItem + "|class|primary} that were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}", "Give me the quantity of {" + inventoryItem + "|class|primary} that were {" + attributeItem +"|modification|attribute} at {" + timeItem+ "|sys_time}"]
   for iteration in range(number):
@@ -86,55 +96,94 @@ def cat4(number):
 def cat5(number):
   data = []
   inventoryItem = inventory[random.randint(0, len(inventory)-1)]
-  attributeItem = attribute[random.randint(0, len(inventory)-1)]
+  attributeItem = attribute[random.randint(0, len(attribute)-1)]
   timeItem = "Jan 1, 2021"
   questions = ["How many {" + attributeItem + " |modification|attribute} occured on {" + inventoryItem + "|class|primary} at {" + timeItem + "|sys_time}", "Which {" + inventoryItem + "|class|primary}did the {" + attributeItem + "|modification|attribute} occur on at {" + timeItem + "|sys_time}", "Tell me the number of {"  + attributeItem + "|modification|attribute} that occured on {" + inventoryItem + "|class|primary} at {" + timeItem + "|sys_time}", "Show me how many {" + attributeItem + "|modification|attribute} happened to the {" + inventoryItem + "|class|primary} at {" + timeItem + "|sys_time}", "Report to me how many {" + attributeItem + "|modification|attribute} happened to {" + inventoryItem + " |class|primary} at {" + timeItem + "|sys_time}", "Display the {" + inventoryItem + "|class|primary} which {" + attributeItem + "|modification|attribute} happened to at {" + timeItem + "|sys_time}"]
   for iteration in range(number):
     data.append(questions[random.randint(0, len(questions)-1)])
   return data
-def genQuestions(category1=0, category2=0, category3=0, category4=0, category5=0, objectParam="none", objectList=[], dateType=4):
+def genQuestions(category1=0, category2=0, category3=0, category4=0, category5=0, objectParam="none", objectList=[], dateType=5):
   if category1 == 0 and category2 == 0 and category3 == 0 and category4 == 0 and category5 == 0:
     print("genQuestions(category1, category2, category3, category4, category5, objectParam, objectList, dateType) \n\ncategory1[int] - how many category 1 questions will be generated\ncategory2[int] - how many category 2 questions will be generated\ncategory3[int] - how many category 3 questions will be generated\ncategory4[int] - how many category 4 questions will be generated\ncategory5[int] - how many category 5 questions will be generated\nobjectParam[str] - include objects. can be 'none', 'random', or 'given'\nobjectList - if objectParam is 'given', the generator will use this list to iterate through questions\ndateType[int] - format of dates. 1 is MM/DD/YYYY. 2 is DD/MM/YYYY. 3 is YYYY/MM/DD. 4 is MM DD, YYYY\n\nSee README.md for more information.")
+  output = []
   if objectParam == "none":
     if category1 != 0:
       print("Category 1 Questions:")
       for phrase in cat1(category1, objectParam, dateType):
         print(phrase)
+        output.append(phrase)
     if category2 != 0:
       print("Category 2 Questions:")
       for phrase in cat2(category2):
         print(phrase)
+        output.append(phrase)
     if category3 != 0:
       print("Category 3 Questions:")
       for phrase in cat3(category3):
         print(phrase)
+        output.append(phrase)
     if category4 != 0:
       print("Category 4 Questions:")
       for phrase in cat4(category4):
         print(phrase)
+        output.append(phrase)
     if category5 != 0:
       print("Category 5 Questions:")
       for phrase in cat5(category5):
         print(phrase)
+        output.append(phrase)
   elif objectParam == "random":
     if category1 != 0:
       print("Category 1 Questions:")
       for phrase in cat1(category1, objectParam, dateType):
         print(phrase)
+        output.append(phrase)
     if category2 != 0:
       print("Category 2 Questions:")
       for phrase in cat2(category2):
         print(phrase)
+        output.append(phrase)
     if category3 != 0:
       print("Category 3 Questions:")
       for phrase in cat3(category3):
         print(phrase)
+        output.append(phrase)
     if category4 != 0:
       print("Category 4 Questions:")
       for phrase in cat4(category4):
         print(phrase)
+        output.append(phrase)
     if category5 != 0:
       print("Category 5 Questions:")
       for phrase in cat5(category5):
         print(phrase)
+        output.append(phrase)
+  elif objectParam == "given":
+    if category1 != 0:
+      print("Category 1 Questions:")
+      for phrase in cat1(category1, objectParam, dateType):
+        print(phrase)
+        output.append(phrase)
+    if category2 != 0:
+      print("Category 2 Questions:")
+      for phrase in cat2(category2):
+        print(phrase)
+        output.append(phrase)
+    if category3 != 0:
+      print("Category 3 Questions:")
+      for phrase in cat3(category3):
+        print(phrase)
+        output.append(phrase)
+    if category4 != 0:
+      print("Category 4 Questions:")
+      for phrase in cat4(category4):
+        print(phrase)
+        output.append(phrase)
+    if category5 != 0:
+      print("Category 5 Questions:")
+      for phrase in cat5(category5):
+        print(phrase)
+        output.append(phrase)
+  return output
+        
 genQuestions(category1=3, category3=1 ,objectParam="random", dateType=3)
